ecdd9482bf891f257eb991135c7991a6a25cceca
Max DeSantis <desantis.max.a@gmail.com>
2022-03-15 16:16:54 -0500

    init lab 5

---
+++ 5_dodgeball/pseudocode.py
@@ -0,0 +1,15 @@
+
+from enum import Enum
+
+class Robot:
+
+    # Define robot states
+    def State(enum):
+
+
+    def __init__(self):
+        print("init")
+
+
+    def handle_pose(self, msg):
+        print("handle_pose")
