ab5e938a0286874d7835971696f838baa6e5a5ce
Max DeSantis <desantis.max.a@gmail.com>
2022-04-06 16:23:42 -0500

    Improved camera, worked on visualizer

--- 6_soccer/catkin_ws/src/soccerbot/scripts/object_finder.py
+++ 6_soccer/catkin_ws/src/soccerbot/scripts/object_finder.py
@@ -27,14 +27,14 @@ class Detector:
         self.ball_stamp_prev = -1
         self.upper_bound = 70
         self.lower_bound = 200
-        self.kernel_size = 29
+        self.kernel_size = 57
 
         # ball detection
-        self.ball_hue_upper = 70
+        self.ball_hue_upper = 40
         self.ball_hue_lower = 0
-        self.ball_sat_lower = 130
-        self.ball_val_lower = 130
-        self.ball_kernel_size = 29
+        self.ball_sat_lower = 110
+        self.ball_val_lower = 120
+        self.ball_kernel_size = 25
         self.ball_erode_iterations = 1
         self.ball_dilate_iterations = 1
         self.ball_width_meters = .3 #meters
@@ -47,9 +47,10 @@ class Detector:
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
-    # Determine ball pose (odom frame) and return it
+    # Determine ball bearing and min/max bounding values
     def locate_ball(self, im_hsv):
 
+        # Find pixels of correct color
         detected_pixels = np.where( (im_hsv[:, :, 0] <= self.ball_hue_upper) & 
             (im_hsv[:, :, 0] >= self.ball_hue_lower) & 
             (im_hsv[:, :, 1] >= self.ball_sat_lower) & 
@@ -60,16 +61,14 @@ class Detector:
         rows, cols = detected_pixels
         kernel = np.ones((self.ball_kernel_size, self.ball_kernel_size), np.uint8)
         im_hsv[rows, cols] = (255, 0, 0)
-        #im_mono = cv2.cvtColor(im_hsv, cv2.CV_HSV2GRAY)
         im_bgr_intermediate = cv2.cvtColor(im_hsv, cv2.COLOR_HSV2BGR)
         im_mono = cv2.cvtColor(im_bgr_intermediate, cv2.COLOR_BGR2GRAY)
-        #im_mono = im_hsv[2]
         im_mono = cv2.erode(im_mono, kernel, iterations = self.ball_erode_iterations)
         im_mono = cv2.dilate(im_mono, kernel, iterations = self.ball_dilate_iterations)
         
-        
+        # With filtered blob, we can be reasonably confident that there is minimal noise.
         minV = maxV = -1
-        if len(cols) >= 600: #passes the test
+        if len(cols) >= 200: #passes the test
             sum = 0
             minV = cols[0]
             maxV = cols[0]
@@ -87,9 +86,9 @@ class Detector:
         else:
             bearing = -1
 
-        self.ball_width_cols = maxV - minV # Not sure if this is correct
-        self.ball_bearing = bearing
+        self.ball_width_cols = maxV - minV
 
+        self.ball_bearing = bearing
         self.ball_detected_pub.publish(self.bridge.cv2_to_imgmsg(im_mono, "mono8"))
 
     def locate_opponent(self, image_hsv):
@@ -109,7 +108,6 @@ class Detector:
             print(e)# Not sure if this is going to work as intended.
 
         # Blur image
-        #kernel = np.ones((self.kernel_size, self.kernel_size), np.float32) / (self.kernel_size * self.kernel_size)
         im_hsv_blurred = cv2.GaussianBlur(im_hsv, (self.kernel_size, self.kernel_size), 0)
         
         ball_bearing = self.locate_ball(im_hsv_blurred)
@@ -117,47 +115,45 @@ class Detector:
         # Publish filtered image of ball
         self.impub.publish(self.bridge.cv2_to_imgmsg(image, "bgr8"))
 
-        # Find the average column of the bright yellow pixels
-        # and store as self.bearing. Store -1 if there are no
-        # bright yellow pixels in the image.
 
     def handle_scan(self, msg):
         if self.ball_bearing >= 0:
             dist = msg.ranges[-self.ball_bearing]
             if not math.isnan(dist):
                 self.ball_distance = dist
-
                 self.ball_stamp = msg.header.stamp
-                
             else:
                 self.ball_distance = -1
         else:
             self.ball_distance = -1
-        #If the bearing is valid, store the corresponding range
-        #in self.distance. Decide what to do if range is NaN.
 
     def build_pose_stamped(self, bearing, distance, stamp):
         pose = PoseStamped()
         pose.header.frame_id = "camera_depth_frame"
         pose.header.stamp = stamp
         
-        # Convert bearing and distance to Pose in camera_frame
-        meters_per_pixel = self.ball_width_meters / self.ball_width_cols
-        ball_disp = (bearing - 320) * meters_per_pixel
-        
-        #print(bearing, meters_per_pixel, ball_disp, distance, ball_disp/distance)
-        ball_theta = math.asin(ball_disp / distance)
-        print("disp: ", ball_disp, "mpp: ", meters_per_pixel, "theta: ", ball_theta * 180/math.pi, "cols: ", self.ball_width_cols)
-        pose.pose.position.x = math.cos(ball_theta) * distance
-        pose.pose.position.y = math.sin(ball_theta) * distance
-        pose.pose.position.z = 0
-        
-        # Transform to odom frame
-        try:
-            transform = self.tf_buffer.lookup_transform("odom", pose.header.frame_id, pose.header.stamp, rospy.Duration(1.0))
-            odomPose = tf2_geometry_msgs.do_transform_pose(pose, transform)
-            self.ball_pose_odom_prev = odomPose
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+
+        if self.ball_width_cols > 0 and distance > 0 and 0 <= bearing <= 640:
+            # Convert bearing and distance to Pose in camera_frame
+            meters_per_pixel = self.ball_width_meters / self.ball_width_cols
+            ball_disp = (bearing - 320) * meters_per_pixel
+            
+            #print(bearing, meters_per_pixel, ball_disp, distance, ball_disp/distance)
+            ratio = max(1, min(-1, ball_disp / distance))
+            ball_theta = math.asin(ratio)
+            #print("disp: ", ball_disp, "mpp: ", meters_per_pixel, "theta: ", ball_theta * 180/math.pi, "cols: ", self.ball_width_cols)
+            pose.pose.position.x = math.cos(ball_theta) * distance
+            pose.pose.position.y = math.sin(ball_theta) * distance
+            pose.pose.position.z = 0
+            
+            # Transform to odom frame
+            try:
+                transform = self.tf_buffer.lookup_transform("odom", pose.header.frame_id, pose.header.stamp, rospy.Duration(1.0))
+                odomPose = tf2_geometry_msgs.do_transform_pose(pose, transform)
+                self.ball_pose_odom_prev = odomPose
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+                odomPose = self.ball_pose_odom_prev
+        else:
             odomPose = self.ball_pose_odom_prev
         
         return odomPose
@@ -166,7 +162,7 @@ class Detector:
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             rate.sleep()
-            
+
             if not self.ball_stamp == self.ball_stamp_prev:
                 ball_pose = PoseStamped()
                 ball_pose = self.build_pose_stamped(self.ball_bearing, self.ball_distance, self.ball_stamp)
--- 6_soccer/catkin_ws/src/soccerbot/scripts/visualizer.py
+++ 6_soccer/catkin_ws/src/soccerbot/scripts/visualizer.py
@@ -7,14 +7,15 @@ from sensor_msgs.msg import Image
 import roslib
 import rospy
 import numpy as np
+import math
 
 class SoccerVisualizer:
 
     def __init__(self, game_state):
         print("init visualizer")
         
-        self.image_width = 100
-        self.image_height = 100
+        self.image_width = 100 # Number of cols
+        self.image_height = 100 # Number of rows
         self.gameState = game_state
         
         self.bridge = CvBridge()
@@ -30,16 +31,24 @@ class SoccerVisualizer:
         #blank[:, 0:self.image_width//2] = (255, 0, 0)
         #blank[:, self.image_width//2:self.image_width] = (0, 255, 0)
         
+        # COLS ARE X, ROWS ARE Y
         ball_x = int(self.gameState.ball_x*10 - self.image_width/2)
         ball_y = int(self.gameState.ball_y*10 - self.image_height/2)
         
+        
+        # This is all screwed up the way Numpy and opencv handle image definitions. Need to redo it.
         bot_x = int(self.gameState.soccerbot_x * 10 - self.image_width/2)
         bot_y = int(self.gameState.soccerbot_y * 10 - self.image_height/2)
+        pointer_x = int(self.gameState.soccerbot_x*10 + math.cos(self.gameState.soccerbot_theta + math.pi/2) * 2 - self.image_width/2)
+        pointer_y = int(self.gameState.soccerbot_y*10 + math.sin(self.gameState.soccerbot_theta + math.pi/2) * 2 - self.image_height/2)
+        
+        blank[int(self.image_width/2), int(self.image_height/4)] = (0, 0, 255)
         
         blank[int(self.image_width/2),int(self.image_height/2)] = (255, 255, 255) # make center white
         
-        blank[ball_x, ball_y] = (255, 255, 150) # ball position is blue
+        blank[ball_y, ball_x] = (255, 255, 150) # ball position is blue
         
-        blank[bot_x, bot_y] = (0, 255, 0) # robot position is green
+        blank[bot_y, bot_x] = (0, 255, 0) # robot position is green
+        blank[pointer_y, pointer_x] = (155, 155, 0)
         
         self.image_pub.publish(self.bridge.cv2_to_imgmsg(blank, "bgr8"))
